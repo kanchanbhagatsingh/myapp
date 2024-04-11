c5ee9cedc0d96c9bb1e5243fd37fa607b07db859
Ashish <ashishdangi96@gmail.com>
2024-04-11 11:31:44 +0530

    recycler view added

---
+++ app/src/main/java/com/example/myfirstapp/ItemsAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.myfirstapp
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter(applicationContext: Context, arrayList: ArrayList<RecyclerModel>) : RecyclerView.Adapter<ItemsAdapter.myViewHolder>() {
+
+    private var arrayList: ArrayList<RecyclerModel>
+    private var context : Context
+
+    // initialize the variables
+    init {
+        this.arrayList = arrayList
+        this.context = applicationContext
+    }
+
+    class myViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var logo : ImageView
+        var title : TextView
+        var desc : TextView
+        var price : TextView
+
+        init {
+            logo = itemView.findViewById(R.id.ivLogo)
+            title = itemView.findViewById(R.id.tvTitle)
+            desc = itemView.findViewById(R.id.tvDesc)
+            price = itemView.findViewById(R.id.cashback)
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
+       val view : View = LayoutInflater.from(parent.context).inflate(R.layout.item_recyclerview, parent, false)
+        return myViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    override fun onBindViewHolder(holder: myViewHolder, position: Int) {
+        val recyclerModel = arrayList[position]
+
+        Log.d("ASHISH KR", "onBindViewHolder: ${recyclerModel.title}")
+
+        holder.logo.setImageResource(recyclerModel.logo)
+        holder.title.text = recyclerModel.title
+        holder.desc.text = recyclerModel.desc
+
+        holder.price.text = recyclerModel.cashback
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/ModelClass.kt
@@ -0,0 +1,17 @@
+package com.example.myfirstapp
+
+class RecyclerModel(logo: Int, title: String, desc: String, cashback: String) {
+    // create model class for storing & populate the data on recyclerview
+     var title : String = ""
+     var desc : String = ""
+     var logo : Int = 0
+     var cashback : String = ""
+
+    init {
+        this.desc = desc
+        this.title = title
+        this.logo = logo
+        this.cashback = cashback
+    }
+
+}
