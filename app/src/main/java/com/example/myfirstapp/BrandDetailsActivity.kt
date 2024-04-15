414920d9dda3bbecbf48276c7610194cb9f254ed
kanchan Singh <kanchansingh77721@gmail.com>
2024-04-15 18:09:25 +0530

    New Changes again

--- app/src/main/java/com/example/myfirstapp/BrandDetailsActivity.kt
+++ app/src/main/java/com/example/myfirstapp/BrandDetailsActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class veromoda : AppCompatActivity() {
+class BrandDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_veromoda)
+        setContentView(R.layout.activity_brand_details)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
